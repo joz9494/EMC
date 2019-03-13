db2bfd86303a2f41337b11a5eabea087d0d4a828
Josue <joz9494@gmail.com>
2019-03-13 13:06:25 -0600

    Inicio
    Update carrousel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -126,13 +126,13 @@
    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" id="imgcarrousel" src="../Recursos/img/11.png" alt="First slide">
+      <img class="d-block w-100" id="imgcarrousel" src="../Recursos/img/Inicio/11.png" alt="First slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" id="imgcarrousel" src="../Recursos/img/11.png" alt="Second slide">
+      <img class="d-block w-100" id="imgcarrousel" src="../Recursos/img/Inicio/11.png" alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" id="imgcarrousel" src="../Recursos/img/11.png" alt="Third slide">
+      <img class="d-block w-100" id="imgcarrousel" src="../Recursos/img/Inicio/11.png" alt="Third slide">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
